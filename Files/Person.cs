7cf6b6752a4081a036d4e66ce7d87b9ddcb1bb78
oleg fruntov <fruntovoleg@gmail.com>
2020-03-23 17:52:10 +0200

    Добавьте файлы проекта.

---
+++ Files/Person.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Files
+{
+    [Serializable]
+    public class Person
+    {
+        //[NonSerialized]
+        //private int? bankAcc;
+
+        [JsonProperty("ИД")]
+        public int Id { get; set; }
+
+        [JsonProperty("Имя")]
+        public string FirstName { get; set; }
+
+        [JsonProperty("Фамилия")]
+        public string LastName { get; set; }
+
+        [JsonProperty("Дата рождения")]
+        public DateTime Birthday { get; set; }
+
+        [JsonIgnore]        
+        public int? BankAccount { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Id}\t{FirstName}\t{LastName}\t{Birthday.ToShortDateString()}\t{BankAccount}";
+        }
+
+        /*
+                     {
+               "Имя": "Иван",
+               "Фамилия": "Иванов",
+               "address": {
+                   "streetAddress": "Московское ш., 101, кв.101",
+                   "city": "Ленинград",
+                   "postalCode": 101101
+               },
+               "phoneNumbers": [
+                   "812 123-1234",
+                   "916 123-4567"
+               ]
+            }
+         */
+    }
+}
---
+++ Files/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+
+namespace Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Drive
+
+            DriveInfo[] drives = DriveInfo.GetDrives();
+
+            // Directory
+
+            string path = @"D:\Test";
+            string subPath = @"MyApp";
+
+            DirectoryInfo dir = new DirectoryInfo(path);
+            try
+            {
+                if (!dir.Exists)
+                {
+                    dir.Create();
+                    dir.CreateSubdirectory(subPath);
+                }                
+            }
+            catch (Exception)
+            {
+               
+            }
+
+            var subDirs = dir.GetDirectories();
+
+            foreach (var item in subDirs)
+            {
+                //Console.WriteLine(item.Name);
+            }
+
+            //dir.Delete(true);
+
+
+            // File
+
+            FileInfo file = new FileInfo(path + @"\test.txt");
+            file.Create();            
+            //file.Delete();   
+
+            using (StreamWriter sw = new StreamWriter(new FileStream("write_test.txt", FileMode.Append)))
+            {
+                while (true)
+                {
+                    string input = Console.ReadLine();
+
+                    if (input == "")
+                    {
+                        break;
+                    }
+
+                    sw.WriteLine(input + " " + DateTime.Now.ToShortTimeString());
+                }
+            }
+
+            //sw.Dispose();
+
+            using(StreamReader sr = new StreamReader("write_test.txt"))
+            {
+                //string output2 = sr.ReadToEnd();
+                //Console.WriteLine(output2);
+                
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            
+            // Serialize
+
+            var p = new Person() { Id = 1, FirstName = "John", LastName = "Doe", Birthday = DateTime.Now, BankAccount = 2358721 };
+
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            //using (FileStream fs = new FileStream("person.bin", FileMode.OpenOrCreate))
+            //{
+            //    formatter.Serialize(fs, p);
+            //}
+
+            //Person person = null;
+
+            //using (FileStream fs = new FileStream("person.bin", FileMode.OpenOrCreate))
+            //{
+            //    person = (Person)formatter.Deserialize(fs);
+            //}
+
+            //XmlSerializer xml = new XmlSerializer(typeof(Person));
+
+            //using (FileStream fs = new FileStream("person.xml", FileMode.OpenOrCreate))
+            //{
+            //    xml.Serialize(fs, p);
+            //}
+
+            //using (FileStream fs = new FileStream("person.xml", FileMode.OpenOrCreate))
+            //{
+            //    var person = xml.Deserialize(fs);
+            //    Console.WriteLine(person);
+            //}
+
+            string output = JsonConvert.SerializeObject(p);
+
+            Console.WriteLine(output);
+
+            var person = JsonConvert.DeserializeObject<Person>(output);
+            Console.WriteLine(person);
+        }
+    }
+}
